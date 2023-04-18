7f84535f344d6a8e6ef8c99114c7be9ff7a34db0
WNS <60368607+yashka072@users.noreply.github.com>
2023-04-18 05:16:32 +0300

    reverse

--- codeby.games/Reverse/(resolved) Функциональный ад.py
+++ codeby.games/Reverse/(resolved) Функциональный ад.py
@@ -1,11 +1,10 @@
+# resolved
 # coding=utf-8
-import string
 import re
 
 # File data:
 data = [48, 34, 61, 72, 92, 75, 85, 86, 49, 19, 17, 84, 36, 78, 20, 79, 32, 64, 39, 14, 62, 91, 11, 21, 29]
 # Exe file contains --> pasrsing to .asm file
-
 
 count = 0
 obj = 0
@@ -16,24 +15,32 @@ with open('Функциональный ад.asm') as f:
         count += 1
         if re.search("""(?:([\"]))""" + "." + """(?:([\"]))""", line) or re.search(
                 """(?:([\"]))""" + "." + "." + """(?:([\"]))""", line):
-            # if obj == 1:
-            #     if count - key[obj][obj] < 5:
-            #         obj = 0
-            #         key[obj][obj+1] = count
-            #         key[obj + 1][obj+1] = line[-5:]
-            #     obj = 0
             if obj == 1 and (count - key[len(key) - 2]) <= 5:
                 key.append(count)
-                key.append(line[-5:])
+                if "\"" not in line[-4:-2]:
+                    key.append(line[-4:-2])
+                else:
+                    key.append(line[-3:-2])
                 obj = 0
+                continue
+            elif obj == 1 and len(key) > 0:
+                key.pop(len(key) - 1)
+                key.pop(len(key) - 1)
+                obj = 0
+
             else:
                 obj = 0
             obj += 1
             key.append(count)
-            print(line[-5:])
-            key.append(line[-5:])
-            # key[obj][obj] = count
-            # key[obj + 1][obj] = line[-5:]
-            # obj += 1
+            if "\”" not in line[-4:-2]:
+                key.append(line[-4:-2])
+            else:
+                key.append(line[-5:-2])
 
-print(key)
+for i in range(len(data)):
+    for j in range(len(key)):
+        if str(data[i]) == key[j]:
+            print(key[j + 2], end='')
+
+
+
