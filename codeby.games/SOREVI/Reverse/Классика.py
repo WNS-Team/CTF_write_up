30a67b883e2f70d6991bcfcb17a685fe9f46618d
Alex Danielov <alex.danielov.work@gmail.com>
2023-04-20 13:23:40 +0300

    D

---
+++ codeby.games/SOREVI/Reverse/Классика.py
@@ -0,0 +1,22 @@
+
+def sub(a1,a2,a3,a4):
+    for i in range(20):
+        result = i
+        if (i >= a2):
+            break
+        if (v5 >= a4 or i <= 0):
+            v5 = 0
+        else:
+            v5+=1
+         (i + a1) ^= (v5 + a3)
+
+    return result
+
+
+s2 = "Symphony 40"
+v3 = len(s2)
+
+sub(&s1, 19, s2, v3)
+
+# if ( !strncmp(&s1, s, 19):
+#     print("good")
