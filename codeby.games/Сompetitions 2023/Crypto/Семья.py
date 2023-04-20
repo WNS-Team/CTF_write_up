110348d96b28315ab0ee84256261b4290e196ccf
WNS <60368607+yashka072@users.noreply.github.com>
2023-04-20 23:06:21 +0300

    codeby

---
+++ codeby.games/Сompetitions 2023/Crypto/Семья.py
@@ -0,0 +1,12 @@
+# resolved
+
+from Crypto.Util.number import long_to_bytes
+
+print(str(long_to_bytes(1129268293))[2:-1], end='')  # 1
+print(str(long_to_bytes(4348283))[2:-1], end='')  # 2
+print('w0w_wh0l3_', end='')  # 3
+part_4 = [102, 97, 109, 49, 108, 121, 95, 116, 48, 95]  # 4
+for i in part_4:
+    print(chr(i), end='')
+part_5 = 'gath3r}'
+print(part_5)
